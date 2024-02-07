6f1c2a9024cd2bff599e06ea980e54a3b8a0c02d
aimane yousr <aimaneyousr@gmail.com>
2024-02-07 19:53:37 +0100

    class BaseModel defines all common attributes/methods for other classes

---
+++ models/base_model.py
@@ -0,0 +1,31 @@
+"""
+class for BaseModel
+"""
+
+import uuid
+from datetime import datetime
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
+
+class BaseModel:
+    """BaseModel class that defines all common attributes/methods for other classes"""
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return the string representation of the BaseModel instance"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Updates the public instance attribute updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__ of the instance"""
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
+        new_dict["created_at"] = new_dict["created_at"].strftime(time)
+        new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+        return new_dict
