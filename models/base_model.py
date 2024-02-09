a5ce80a5c789faf66ccc0eda1128e13caad11db7
aimane yousr <aimaneyousr@gmail.com>
2024-02-09 13:47:55 +0100

    fixing pycodestyle of class BaseModel that defines all common attributes/methods for other classes

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 """
-class for BaseModel
+Class for BaseModel
 """
 
 import uuid
@@ -7,23 +7,35 @@ from datetime import datetime
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
+
 class BaseModel:
-    """BaseModel class that defines all common attributes/methods for other classes"""
+    """
+    BaseModel class that defines all common
+    attributes/methods for other classes
+    """
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        """Return the string representation of the BaseModel instance"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """
+        Return the string representation of the BaseModel instance
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Updates the public instance attribute updated_at with the current datetime"""
+        """
+        Updates the public instance attribute updated_at
+        with the current datetime
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values of __dict__ of the instance"""
+        """
+        Returns a dictionary containing all keys/values
+        of __dict__ of the instance
+        """
         new_dict = self.__dict__.copy()
         new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = new_dict["created_at"].strftime(time)
