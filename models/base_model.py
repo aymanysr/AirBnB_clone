f351e7bef3e2a1ddc79285e7c2e2cd9605eb2c91
aimane yousr <aimaneyousr@gmail.com>
2024-02-09 14:24:55 +0100

    update base_model to use *args, **kwargs arguments for the constructor

--- models/base_model.py
+++ models/base_model.py
@@ -13,10 +13,18 @@ class BaseModel:
     BaseModel class that defines all common
     attributes/methods for other classes
     """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """initializes the BaseModel instance"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, time)
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
