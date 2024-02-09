cc0859a253756dfa1351b3e68e5bf5d19e7d4c56
aimane yousr <aimaneyousr@gmail.com>
2024-02-09 19:26:02 +0100

    add class FileStorage that serializes instances to a JSON file and deserializes JSON file to instances

---
+++ models/engine/file_storage.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+Class for FileStorage
+"""
+
+from calendar import c
+import json
+from models.base_model import BaseModel
+
+class_dict = {"BaseModel": BaseModel}
+
+
+class FileStorage:
+    """
+    FileStorage class that serial and deserializes instances
+    to a JSON file and vice versa
+    """
+
+    __file_path = "file.json"  # path to the JSON file
+    __objects = {}  # empty dictionary to store all objects by <class name>.id
+
+    def all(self):
+        """
+        Returns the dictionary __objects
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        Sets in __objects the obj with key <obj class name>.id
+        """
+        if obj is not None:
+            key = obj.__class__.__name__ + "." + obj.id
+            self.__objects[key] = obj
+
+    def save(self):
+        """
+        Serializes __objects to a JSON file (path: __file_path)
+        """
+        json_objs = {}
+        for key, value in self.__objects.items():
+            json_objs[key] = value.to_dict()
+
+        with open(self.__file_path, 'w') as f:
+            json.dump(json_objs, f)
+
+    def reload(self):
+        """
+        deserializes the JSON file to __objects
+        (only if the JSON file exists; otherwise, do nothing)
+        """
+        try:
+            with open(self.__file_path, 'r') as f:
+                json_obj = json.load(f)
+
+            for key, value in json_obj.items():
+                self.__objects[key] = class_dict[value["__class__"]](**value)
+
+        except FileNotFoundError:
+            pass
