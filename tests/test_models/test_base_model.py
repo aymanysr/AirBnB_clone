8198b08816452f0756b3d902262864acc20d20fa
aimane yousr <aimaneyousr@gmail.com>
2024-02-10 22:12:37 +0100

    tests folder

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Unittest for the BaseModel class"""
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+    """Test for the BaseModel class"""
+    def test_init(self):
+        """Test for the __init__ method"""
+        pass
+
+    def test_datetime_attr(self):
+        """Test for the datetime attributes"""
+        pass
+
+    def test_uuid(self):
+        """Test is id is a valid uuid"""
+        pass
+
+    def test_to_dict(self):
+        """Test for the to_dict method"""
+        pass
+
+    def test_str(self):
+        """Test for the __str__ method"""
+        pass
+
+    def test_save(self):
+        """Test for the save method"""
+        pass
