50761a4898b17cea65fd4b64336bd6e29311d5d4
aimane yousr <aimaneyousr@gmail.com>
2024-02-11 17:00:57 +0100

    Add new model classes to class_dict

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,8 +6,14 @@ Class for FileStorage
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
-class_dict = {"BaseModel": BaseModel, "User": User}
+class_dict = {"BaseModel": BaseModel, "User": User, "State": State, "City": City,
+              "Place": Place, "Amenity": Amenity, "Review": Review}
 
 
 class FileStorage:
