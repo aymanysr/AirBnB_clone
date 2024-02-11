ce7fbcd7dbe53c386fa748758752a9f97fdbde7b
aimane yousr <aimaneyousr@gmail.com>
2024-02-11 17:01:34 +0100

    Add unit tests for file storage functionality

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -45,6 +45,37 @@ class TestFileStorage(unittest.TestCase):
         new_objects = self.fs.all()
         self.assertEqual(objects, new_objects)
 
+    def test_all_returns_dict(self):
+        """Test that all returns a dictionary"""
+        self.assertIsInstance(self.fs.all(), dict)
+
+    def test_new_adds_object(self):
+        """Test that new adds an object to the storage"""
+        new_model = BaseModel()
+        self.fs.new(new_model)
+        self.assertIn("BaseModel." + new_model.id, self.fs.all())
+
+    def test_save_creates_file(self):
+        """Test that save creates a file"""
+        self.fs.save()
+        self.assertTrue(os.path.exists("file.json"))
+
+    def test_reload_loads_objects(self):
+        """Test that reload loads objects from the file"""
+        objects_before = self.fs.all()
+        self.fs.reload()
+        objects_after = self.fs.all()
+        self.assertEqual(objects_before, objects_after)
+
+    def test_reload_ignores_missing_file(self):
+        """Test that reload doesn't raise an exception if the file doesn't exist"""
+        if os.path.exists("file.json"):
+            os.remove("file.json")
+        try:
+            self.fs.reload()
+        except FileNotFoundError:
+            self.fail("reload raised FileNotFoundError unexpectedly!")
+
 
 if __name__ == '__main__':
     unittest.main()
