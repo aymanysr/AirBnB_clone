2fb78258d194f7c2e196ed6948019f107eb13cab
aimane yousr <aimaneyousr@gmail.com>
2024-02-12 15:18:19 +0100

    Update datetime to use UTC in BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -29,7 +29,7 @@ class BaseModel:
         else:
             # If it's a new instance
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.created_at = self.updated_at = datetime.utcnow()
             models.storage.new(self)  # add the instance to the dictionary
 
     def __str__(self):
@@ -43,7 +43,7 @@ class BaseModel:
         Updates the public instance attribute updated_at
         with the current datetime
         """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
